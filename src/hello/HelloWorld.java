379edafa811286a0530f3365737da3cd779b1708
Abhinav Raj <abhinavraj@Abhinavs-MacBook-Air.local>
2022-11-12 03:02:47 +0530

    First commit for Hello

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println(2+3+" is the sum");
+		System.out.println("the sum is "+2+3);
+		
+	}
+
+}
